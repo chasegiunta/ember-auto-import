e3b38fb558df8c697a2c326312e247eb70729342
Edward Faulkner <ef@alum.mit.edu>
2021-03-28 23:44:05 -0400

    add ember-source to our scenarios

--- test-scenarios/fastboot-helper.ts
+++ test-scenarios/fastboot-helper.ts
@@ -7,10 +7,24 @@ export async function setupFastboot(app: PreparedApp, environment = 'development
     throw new Error(`failed to build app for fastboot: ${result.output}`);
   }
 
+  let logs: any[] = [];
+
   const FastBoot = require('fastboot');
   let fastboot = new FastBoot({
     distPath: join(app.dir, 'dist'),
     resilient: false,
+    buildSandboxGlobals(defaultGlobals: any) {
+      return Object.assign({}, defaultGlobals, {
+        console: {
+          log(...args: any[]) {
+            logs.push(args);
+          },
+          warn(...args: any[]) {
+            logs.push(args);
+          },
+        },
+      });
+    },
   });
   async function visit(url: string) {
     const jsdom = require('jsdom');
@@ -19,5 +33,12 @@ export async function setupFastboot(app: PreparedApp, environment = 'development
     let html = await page.html();
     return new JSDOM(html);
   }
-  return { visit };
+
+  function dumpLogs() {
+    for (let log of logs) {
+      console.log(...log);
+    }
+  }
+
+  return { visit, dumpLogs };
 }
